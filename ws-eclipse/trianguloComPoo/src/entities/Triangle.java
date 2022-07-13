1d163843a1bc554e696d2ff2d6571fa9a920aaa8
Ana Sousa <ana.sousa-ext@dasa.com.br>
2022-07-12 21:11:38 -0300

    Método area() criado

--- ws-eclipse/trianguloComPoo/src/entities/Triangle.java
+++ ws-eclipse/trianguloComPoo/src/entities/Triangle.java
@@ -4,4 +4,10 @@ public class Triangle {
 	public double a;
 	public double b;
 	public double c;
+	
+	public double area() {
+		// não recebe parâmtro porque os parâmtros já são as variáveis declaradas acima
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 }
